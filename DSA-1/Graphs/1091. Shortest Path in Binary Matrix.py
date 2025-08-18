efafd6ba35e87ac722776ef016a40ff2b00382ea
Bishad Deb Roy Utsha <royud11@gmail.com>
2025-08-18 12:48:47 -0300

    1091 Bidirectional BFS

--- DSA-1/Graphs/1091. Shortest Path in Binary Matrix.py
+++ DSA-1/Graphs/1091. Shortest Path in Binary Matrix.py
@@ -27,3 +27,58 @@ class Solution:
             return -1
                 
         return bfs(0, 0) if not grid[0][0] and not grid[R - 1][C - 1] else -1
+    
+    # Bidirectional BFS approach
+    def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
+        R, C = len(grid), len(grid[0])
+        neighbors = [[0, 1], [0, -1], [1, 0], [-1, 0],
+                    [1, 1], [1, -1], [-1, 1], [-1, -1]]
+
+        if grid[0][0] or grid[R - 1][C - 1]:
+            return -1
+        if R == 1 and C == 1:  # Single cell case
+            return 1
+
+        def bidirectional_bfs():
+            start_q = collections.deque([(0, 0)])
+            end_q = collections.deque([(R - 1, C - 1)])
+            
+            start_visit = {(0, 0)}
+            end_visit = {(R - 1, C - 1)}
+
+            length = 2
+            
+            while start_q and end_q:
+                for _ in range(len(start_q)):
+                    r, c = start_q.popleft()
+                    for dr, dc in neighbors:
+                        nr, nc = r + dr, c + dc
+                        if (min(nr, nc) < 0 or nr == R or nc == C
+                            or (nr, nc) in start_visit or grid[nr][nc] == 1):
+                            continue
+                        
+                        if (nr, nc) in end_visit:
+                            return length  # Found meeting point!
+                        
+                        start_q.append((nr, nc))
+                        start_visit.add((nr, nc))
+                
+                for _ in range(len(end_q)):
+                    r, c = end_q.popleft()
+                    for dr, dc in neighbors:
+                        nr, nc = r + dr, c + dc
+                        if (min(nr, nc) < 0 or nr == R or nc == C
+                            or (nr, nc) in end_visit or grid[nr][nc] == 1):
+                            continue
+                        
+                        if (nr, nc) in start_visit:
+                            return length + 1  # Found meeting point! (+1 for end step)
+                        
+                        end_q.append((nr, nc))
+                        end_visit.add((nr, nc))
+                
+                length += 2
+            
+            return -1
+
+        return bidirectional_bfs()
