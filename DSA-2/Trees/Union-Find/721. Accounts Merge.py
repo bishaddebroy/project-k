ca2bfe1f6d5736cf91a6c15a5be7ed1b16ac96e6
Bishad Deb Roy Utsha <royud11@gmail.com>
2025-10-08 20:32:28 -0300

    721 Accounts Merge

---
+++ DSA-2/Trees/Union-Find/721. Accounts Merge.py
@@ -0,0 +1,52 @@
+class UF:
+    def __init__(self, n):
+        self.par = {}
+        self.rank = {}
+
+        for i in range(n):
+            self.par[i] = i
+            self.rank[i] = 1
+    
+    def find(self, n):
+        while n != self.par[n]:
+            self.par[n] = self.par[self.par[n]]
+            n = self.par[n]
+        return n
+    
+    def union(self, n1, n2):
+        p1, p2 = self.find(n1), self.find(n2)
+
+        if p1 == p2:
+            return False
+        if self.rank[p1] > self.rank[p2]:
+            self.par[p2] = p1
+            self.rank[p1] += self.rank[p2]
+        else:
+            self.par[p1] = p2
+            self.rank[p2] += self.rank[p1]
+        return True
+
+
+class Solution:
+    def accountsMerge(self, accounts: List[List[str]]) -> List[List[str]]:
+        uf = UF(len(accounts))
+        emailToAcc = {} # email: index of account
+
+        for i, a in enumerate(accounts):
+            for e in a[1:]:
+                if e in emailToAcc:
+                    uf.union(i, emailToAcc[e])
+                else:
+                    emailToAcc[e] = i
+        
+        accToEmails = defaultdict(list) # index of acc: list of emails
+        for e, i in emailToAcc.items():
+            leader = uf.find(i)
+            accToEmails[leader].append(e)
+        
+        res = []
+        for i, emails in accToEmails.items():
+            name = accounts[i][0]
+            res.append([name] + sorted(emails))
+        
+        return res
